2532109154f1ecd73f0a75f98633cb61572f500a
Cheney Cai <cheneycai2004@hotmail.com>
2023-05-09 10:55:01 -0600

    zoom total supply

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,59 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {  useBlockNumber, useEtherBalance, useEthers } from '@usedapp/core'
 import { formatEther } from '@ethersproject/units'
 import { Button } from '@mui/material';
-import { formatCurrency } from '@usedapp/core/dist/esm/src/model';
+import { utils } from 'ethers';
+import { Contract } from '@ethersproject/contracts';
+
+import zoomArtifactJson from './resources/ZoomToken.json';
+import zoombiesArtifactJson from './resources/Zoombies.json';
 
 function chainName(chainId) {
   if (chainId == 1)  {
-    return "Ethereum Mainnet"
+    return "Ethereum Mainnet";
+  }
+  else if (chainId == 1284) {
+    return "Moonbeam";
+  }
+  else if (chainId == 1285) {
+    return "Moonriver";
   }
   else if (chainId == 1287) {
-    return "Moonbase Alpha"
+    return "Moonbase Alpha";
   }
   else {
-    return "Unconfigured chain"
+    return "Unconfigured chain";
   }
 }
 
 export default function App() {
-  const { account, deactivate, activateBrowserWallet, chainId, error } = useEthers()
+  
+  //connect to wallet and blockchain details
+  const { account, deactivate, activateBrowserWallet, chainId, library } = useEthers()
   const etherBalance = useEtherBalance(account)
   const blockNumber = useBlockNumber();
+
+  //contract variables (zoom)
+  let zoomContract;
+  const [zoomTotalSupply, setZoomTotalSupply] = useState(null);
+  async function UpdateZoomTotalSupply() {
+    setZoomTotalSupply(await zoomContract.totalSupply());
+  }
+
+  //contract variables (zoombies)
+  let zoombiesContract;
+
+  //update contracts
+  if (chainId == 1284 || chainId == 1285 || chainId == 1287) {
+    const zoomWethInterface = new utils.Interface(zoomArtifactJson.abi);
+    const zoomWethContractAddress = zoomArtifactJson.networks[chainId].address;
+    zoomContract = new Contract(zoomWethContractAddress, zoomWethInterface, library);
+    UpdateZoomTotalSupply();
+
+    const zoombiesWethInterface = new utils.Interface(zoombiesArtifactJson.abi);
+    const zoombiesWethContractAddress = zoombiesArtifactJson.networks[chainId].address;
+    zoombiesContract = new Contract(zoombiesWethContractAddress, zoombiesWethInterface, library);
+  }
 
   return (
     <div>
@@ -49,6 +83,30 @@ export default function App() {
 
           <b>Current block number:</b>
           <p>{blockNumber}</p>
+
+          <br />
+        </div>
+      )}
+
+      {zoomContract && (
+        <div>
+          <b>Zoom token contract (total supply):</b>
+          <p>{formatEther(zoomTotalSupply)}</p>
+
+          <br />
+
+          <b>Zoombies token contract (total supply):</b>
+          <p>{}</p>
+
+          <br />
+
+          <b>Zoombies token contract (credits owned):</b>
+          <p>{}</p>
+
+          <br />
+
+          <b>Last card minted:</b>
+          <img src="https://zoombies.world/nft-image/295508" width="10%"/>
 
         </div>
       )}
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Mainnet, DAppProvider, Config, MoonbaseAlpha} from '@usedapp/core';
+import { Mainnet, DAppProvider, Config, MoonbaseAlpha, Moonbeam} from '@usedapp/core';
 
 const config = {
   readOnlyChainId: Mainnet.chainId,
   readOnlyUrls: {
     [Mainnet.chainId]: 'https://eth-mainnet.g.alchemy.com/v2/J038e3gaccJC6Ue0BrvmpjzxsdfGly9n',
     [MoonbaseAlpha.chainId]: 'https://rpc.api.moonbase.moonbeam.network/',
+    [Moonbeam.chainId]: 'https://rpc.api.moonbeam.network/',
   },
   refresh: 'never'
 }
