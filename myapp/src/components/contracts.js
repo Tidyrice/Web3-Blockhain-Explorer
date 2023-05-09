259cb5202b63017a1cc2e4e59729c83e374bdba4
Cheney Cai <cheneycai2004@hotmail.com>
2023-05-09 16:38:50 -0600

    event listeners (buggy)

--- myapp/src/components/contracts.js
+++ myapp/src/components/contracts.js
@@ -6,47 +6,74 @@ import { useEthers } from '@usedapp/core'
 
 import zoomArtifactJson from '../resources/ZoomToken.json';
 import zoombiesArtifactJson from '../resources/Zoombies.json';
+import { SubscribeZoomTransfer, SubscribeZoombiesTransfer, SubscribeZoombiesCardMinted, SubscribeDailyReward, SubscribePackOpened } from './listeners.js';
 
 export default function Contracts() {
 
-    const { chainId, library } = useEthers();
+    const { account, chainId, library } = useEthers();
+
 
     //contract variables (zoom)
     const [zoomTotalSupply, setZoomTotalSupply] = useState(null);
+
     async function UpdateZoomTotalSupply(zoomContract) {
-    setZoomTotalSupply(await zoomContract.totalSupply());
+        setZoomTotalSupply(await zoomContract.totalSupply());
     }
+
 
     //contract variables (zoombies)
     const [zoombiesTotalSupply, setZoombiesTotalSupply] = useState(null);
     const [boosterCredits, setBoosterCredits] = useState(null);
+
     async function UpdateZoombiesTotalSupply(zoombiesContract) {
-    setZoombiesTotalSupply(await zoombiesContract.totalSupply());
+        setZoombiesTotalSupply(await zoombiesContract.totalSupply());
     }
+
     async function UpdateBoosterCredits(zoombiesContract, zoombiesContractAddress) {
-    setBoosterCredits(await zoombiesContract.boosterCreditsOwned(zoombiesContractAddress));
+        setBoosterCredits(await zoombiesContract.boosterCreditsOwned(zoombiesContractAddress));
     }
+
 
     //update contracts
     useEffect (() => {
-    if (chainId === 1284 || chainId === 1285 || chainId === 1287) {
-        const zoomWethInterface = new utils.Interface(zoomArtifactJson.abi);
-        const zoomContractAddress = zoomArtifactJson.networks[chainId].address;
-        const zoomContract = new Contract(zoomContractAddress, zoomWethInterface, library);
-        UpdateZoomTotalSupply(zoomContract);
+        if (chainId === 1284 || chainId === 1285 || chainId === 1287) {
 
-        const zoombiesWethInterface = new utils.Interface(zoombiesArtifactJson.abi);
-        const zoombiesContractAddress = zoombiesArtifactJson.networks[chainId].address;
-        const zoombiesContract = new Contract(zoombiesContractAddress, zoombiesWethInterface, library);
-        UpdateZoombiesTotalSupply(zoombiesContract);
-        UpdateBoosterCredits(zoombiesContract, zoombiesContractAddress);
-    }
-    }, [chainId]);
+            //zoom
+            const zoomWethInterface = new utils.Interface(zoomArtifactJson.abi);
+            const zoomContractAddress = zoomArtifactJson.networks[chainId].address;
+            const zoomContract = new Contract(zoomContractAddress, zoomWethInterface, library);
 
+            UpdateZoomTotalSupply(zoomContract);
+            SubscribeZoomTransfer(zoomContract); //TRANSFER ZOOM
+
+    
+            //zoombies
+            const zoombiesWethInterface = new utils.Interface(zoombiesArtifactJson.abi);
+            const zoombiesContractAddress = zoombiesArtifactJson.networks[chainId].address;
+            const zoombiesContract = new Contract(zoombiesContractAddress, zoombiesWethInterface, library);
+
+            UpdateZoombiesTotalSupply(zoombiesContract);
+            UpdateBoosterCredits(zoombiesContract, zoombiesContractAddress);
+            SubscribeZoombiesTransfer(zoombiesContract); //TRANSFER ZOOMBIES
+            SubscribeZoombiesCardMinted(zoombiesContract); //MINT ZOOMBIES
+            SubscribeDailyReward(zoombiesContract); //DAILY REWARD
+            SubscribePackOpened(zoombiesContract); //PACK OPENED
+
+
+            //clean up active listeners
+            return () => {
+                zoomContract.removeAllListeners();
+                zoombiesContract.removeAllListeners();
+            }
+
+        }
+    }, [chainId, library]);
+
+    
     return (
         <div>
 
-            {zoomTotalSupply && (
+            {account && zoomTotalSupply && (
                 <div>
         
                 <b>Zoom token contract (total supply):</b>
---
+++ myapp/src/components/listeners.js
@@ -0,0 +1,29 @@
+export function SubscribeZoomTransfer(zoomContract) {
+    zoomContract.on("Transfer", (from, to, amount) => {
+        console.log("Transfer event: ", from, to, amount);
+    });
+}
+
+export function SubscribeZoombiesTransfer(zoombiesContract) {
+    zoombiesContract.on("Transfer", (from, to, tokenId) => {
+        console.log("Transfer event: ", from, to, tokenId);
+    });
+}
+
+export function SubscribeZoombiesCardMinted(zoombiesContract) {
+    zoombiesContract.on("LogCardMinted", (owner, tokenId, cardTypeId, editionNumber) => {
+        console.log("CardMinted event: ", owner, tokenId, cardTypeId, editionNumber);
+    });
+}
+
+export function SubscribeDailyReward(zoombiesContract) {
+    zoombiesContract.on("LogDailyReward", (owner, creditsRemaining) => {
+        console.log("DailyReward event: ", owner, creditsRemaining);
+    })
+}
+
+export function SubscribePackOpened(zoombiesContract) {
+    zoombiesContract.on("LogPackOpened", (owner, rarity) => {
+        console.log("PackOpened event: ", owner, rarity);
+    })
+}
